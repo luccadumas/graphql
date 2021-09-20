b8c8b441662e94e5a64d2cdde957190e76d16adb
Lucca Dumas <lucca.ribeiro0@hotmail.com>
2021-09-20 17:10:18 -0300

    feat(main): Working with object and array

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 const { gql, ApolloServer } = require("apollo-server");
 
+const produtos = [
+    {
+        id: 1,
+        nome: 'Notebook',
+        valor: 12000.32
+    },
+    {
+        id: 2,
+        nome: 'TV',
+        valor: 6000.32
+    }
+];
+
+const usuarios = [
+    {
+        id: 1,
+        nome: 'Teste 1',
+        salario: 1234.54,
+        ativo: true,
+        idade: 23
+    },
+    {
+        id: 2,
+        nome: 'Teste 2',
+        salario: 4321.54,
+        ativo: false,
+        idade: 30
+    }
+];
+
+const typeDefs = gql`
+	type Produto {
+		id: ID
+		nome: String
+		valor: Float
+	}
+	type Usuario {
+		idade: Int
+		salario: Float
+		nome: String
+		ativo: Boolean
+		id: ID
+	}
+	type Query {
+		usuarios: [Usuario]
+		produtos: [Produto]
+	}
+`;
 const resolvers = {
     Query: {
-        hello() {
-            return "World"
+        usuarios() {
+            return usuarios;
+        },
+        produtos() {
+            return produtos;
         }
     }
-}
-const typeDefs = gql`
-    type Query {
-        hello: String,
-    }
-`
+};
 
 const server = new ApolloServer({
     typeDefs,

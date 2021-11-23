f116830b4759a5f594d92e952a2ffceba2e39334
Lucca Dumas <lucca.ribeiro0@hotmail.com>
2021-11-23 17:47:47 -0300

    feat(resolvers): Add Fragments & Aliases

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const { gql, ApolloServer } = require("apollo-server");
 
-const db = [
-  {
-    id: 1,
-    nome: "Teste",
-    email: "teste@email.com",
-    telefone: "11 1234 1234",
-    perfil: 1,
-  },
-  {
-    id: 2,
-    nome: "Teste 2",
-    email: "teste2@email.com",
-    telefone: "34 1234 1234",
-    perfil: 2,
-  },
-];
-
-const perfis = [
-  { id: 1, descricao: "ADMIN" },
-  { id: 2, descricao: "NORMAL" },
-];
+const db = {
+  usuarios: [
+    {
+      id: 1,
+      nome: "Usuario 1",
+      email: "usuario1@email.com",
+      telefone: "11 1234 1234",
+      perfil: 1,
+    },
+    {
+      id: 2,
+      nome: "Usuario 2",
+      email: "usuario2@email.com",
+      telefone: "34 1234 1234",
+      perfil: 2,
+    },
+  ],
+  perfis: [
+    { id: 1, descricao: "ADMIN" },
+    { id: 2, descricao: "NORMAL" },
+  ],
+};
 
 const typeDefs = gql`
   type Usuario {
@@ -37,21 +38,23 @@ const typeDefs = gql`
   type Query {
     usuario(id: Int): Usuario
     perfis: [Perfil]
+    usuarios: [Usuario]
   }
 `;
 const resolvers = {
   Usuario: {
     perfil(usuario) {
-      return perfis.find((p) => p.id === usuario.perfil);
+      return db.perfis.find((p) => p.id === usuario.perfil);
     },
   },
   Query: {
     usuario(obj, args) {
-      return db.find((db) => db.id === args.id);
+      return db.usuarios.find((db) => db.id === args.id);
     },
     perfis() {
-      return perfis;
+      return db.perfis;
     },
+    usuarios: () => db.usuarios,
   },
 };
 
@@ -60,16 +63,28 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen(3000);
+server.listen();
 
-// query Query {
-// 	perfis {
-// 	  descricao
-// 	},
-// 	usuario(id: 1) {
-// 	  id,
-// 	  nome,
-// 	  email,
-// 	  telefone
-// 	}
+// apollographql
+
+// query {
+//   primeiroUsuario: usuario(id: 1){
+//     id,
+//     ...usuarioCompleto
+//   }
+//   segundoUsuario: usuario(id: 2){
+//     ...usuarioCompleto
+//   }
+//   usuarios {
+//     ...usuarioCompleto
+//   }
+// }
+
+// fragment usuarioCompleto on Usuario{
+//   nome,
+//   telefone,
+//   perfil {
+//     id,
+//     descricao
+//   }
 // }

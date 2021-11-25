560bdd4c6c3f94272c98326a09e3cbc8cb6501d2
Lucca Dumas <lucca.ribeiro0@hotmail.com>
2021-11-25 17:25:31 -0300

    feat(general): Organizing folders and files

--- index.js
+++ index.js
@@ -1,74 +1,12 @@
-const { gql, ApolloServer } = require("apollo-server");
-
-const db = {
-  usuarios: [
-    {
-      id: 1,
-      nome: "Usuario 1",
-      email: "usuario1@email.com",
-      telefone: "11 1234 1234",
-      perfil: 1,
-    },
-    {
-      id: 2,
-      nome: "Usuario 2",
-      email: "usuario2@email.com",
-      telefone: "34 1234 1234",
-      perfil: 2,
-    },
-  ],
-  perfis: [
-    { id: 1, descricao: "ADMIN" },
-    { id: 2, descricao: "NORMAL" },
-  ],
-};
-
-const typeDefs = gql`
-
-  enum TipoPerfil {
-    ADMIN
-    NORMAL
-  }
-  type Usuario {
-    id: Int
-    nome: String
-    email: String
-    telefone: String
-    perfil: Perfil
-  }
-  type Perfil {
-    id: Int
-    descricao: TipoPerfil
-  }
-  type Query {
-    usuario(id: Int): Usuario
-    perfis: [Perfil]
-    usuarios: [Usuario]
-  }
-`;
-const resolvers = {
-  Usuario: {
-    perfil(usuario) {
-      return db.perfis.find((p) => p.id === usuario.perfil);
-    },
-  },
-  Query: {
-    usuario(obj, args) {
-      return db.usuarios.find((db) => db.id === args.id);
-    },
-    perfis() {
-      return db.perfis;
-    },
-    usuarios: () => db.usuarios,
-  },
-};
+const { ApolloServer } = require("apollo-server");
+const { typeDefs, resolvers } = require("./src/graphql");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-server.listen(3000);
+server.listen(3000).then(({ url }) => { console.log(`Server is started! 🚀 ${url}`); });
 
 // apollographql
 
